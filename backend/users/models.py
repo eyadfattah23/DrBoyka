23a2dfc4d81b85ab27e6e3c342ba1c0d614e2c50
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-12-19 19:45:24 +0200

    ✨ feat(users): add Transformation model
    - implement Transformation model with fields: name, duration, before_image, after_image
    - add __str__ method for representation

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Transformation(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.CharField(max_length=50)
+    before_image = models.ImageField(upload_to='before_images/')
+    after_image = models.ImageField(upload_to='after_images/')
+
+    def __str__(self):
+        return self.name
